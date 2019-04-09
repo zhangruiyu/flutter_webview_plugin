a4e455463f6685d17e0ba24369167f5ca7eac89c
张瑞宇 <15201231801@163.com>
2019-04-09 10:43:38 +0800

    face修改

--- android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
+++ android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.annotation.TargetApi;
 import android.app.Activity;
