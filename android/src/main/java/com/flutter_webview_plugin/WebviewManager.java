0222fada8ec2220587a0d0393d74cbaa08b70604
张瑞宇 <15201231801@163.com>
2019-04-09 10:35:19 +0800

    face修改

--- android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
+++ android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
@@ -2,6 +2,9 @@ package com.flutter_webview_plugin;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.annotation.TargetApi;
 import android.app.Activity;
@@ -19,14 +22,20 @@ import com.tencent.smtt.sdk.WebChromeClient;
 import com.tencent.smtt.sdk.WebView;
 import com.tencent.smtt.sdk.WebViewClient;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
-
+import androidx.core.content.FileProvider;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 
 import static android.app.Activity.RESULT_OK;
+import static android.os.Build.VERSION_CODES.M;
 
 /**
  * Created by lejard_h on 20/12/2017.
@@ -36,30 +45,66 @@ class WebviewManager {
 
     private ValueCallback<Uri> mUploadMessage;
     private ValueCallback<Uri[]> mUploadMessageArray;
-    private final static int FILECHOOSER_RESULTCODE=1;
+    private final static int FILECHOOSER_RESULTCODE = 1;
+
+    public static final int INPUT_FILE_REQUEST_CODE = 100;
+    public static final int INPUT_VIDEO_CODE = 200;
+    private String mCameraPhotoPath;
+    private Uri photoURI;
 
     @TargetApi(7)
     class ResultHandler {
-        public boolean handleResult(int requestCode, int resultCode, Intent intent){
+        public boolean handleResult(int requestCode, int resultCode, Intent intent) {
             boolean handled = false;
-            if(Build.VERSION.SDK_INT >= 21){
-                if(requestCode == FILECHOOSER_RESULTCODE){
+            if (Build.VERSION.SDK_INT >= 21) {
+                if (requestCode == FILECHOOSER_RESULTCODE) {
                     Uri[] results = null;
-                    if(resultCode == Activity.RESULT_OK && intent != null){
+                    if (resultCode == Activity.RESULT_OK && intent != null) {
                         String dataString = intent.getDataString();
-                        if(dataString != null){
-                            results = new Uri[]{ Uri.parse(dataString) };
+                        if (dataString != null) {
+                            results = new Uri[]{Uri.parse(dataString)};
                         }
                     }
-                    if(mUploadMessageArray != null){
+                    if (mUploadMessageArray != null) {
                         mUploadMessageArray.onReceiveValue(results);
                         mUploadMessageArray = null;
                     }
                     handled = true;
+                } else if(requestCode == INPUT_FILE_REQUEST_CODE || requestCode == INPUT_VIDEO_CODE){
+                    if(resultCode == Activity.RESULT_OK){
+                        Uri[] results = null;
+                        Uri mUri = null;
+                        if(requestCode == INPUT_FILE_REQUEST_CODE){
+                            if(intent == null){
+                                if(Build.VERSION.SDK_INT > M) {
+                                    mUri=photoURI;
+                                    results=new Uri[]{mUri};
+                                } else{
+                                    if (mCameraPhotoPath != null) {
+                                        mUri = Uri.parse(mCameraPhotoPath);
+                                        results = new Uri[]{Uri.parse(mCameraPhotoPath)};
+                                    }
+                                }
+                            } else {
+                                Uri nUri = intent.getData();
+                                if (nUri != null) {
+                                    mUri =nUri;
+                                    results = new Uri[]{nUri};
+                                }
+                            }
+                        } else if(requestCode == INPUT_VIDEO_CODE){
+                            mUri = intent.getData();
+                            results = new Uri[]{mUri};
+                        }
+
+                        mUploadMessageArray.onReceiveValue(results);
+                        mUploadMessageArray = null;
+                        handled = true;
+                    }
                 }
-            }else {
+            } else {
                 if (requestCode == FILECHOOSER_RESULTCODE) {
-                	Uri result = null;
+                    Uri result = null;
                     if (resultCode == RESULT_OK && intent != null) {
                         result = intent.getData();
                     }
@@ -68,6 +113,27 @@ class WebviewManager {
                         mUploadMessage = null;
                     }
                     handled = true;
+                } else if(requestCode == INPUT_FILE_REQUEST_CODE || requestCode == INPUT_VIDEO_CODE){
+                    if(resultCode == Activity.RESULT_OK){
+                        Uri mUri = null;
+                        if(requestCode == INPUT_FILE_REQUEST_CODE){
+                            if(intent == null){
+                                if (mCameraPhotoPath != null) {
+                                    mUri = Uri.parse(mCameraPhotoPath);
+                                }
+                            } else {
+                                Uri nUri = intent.getData();
+                                if (nUri != null) {
+                                    mUri =nUri;
+                                }
+                            }
+                        } else if(requestCode == INPUT_VIDEO_CODE){
+                            mUri = intent.getData();
+                        }
+                        mUploadMessage.onReceiveValue(mUri);
+                        mUploadMessage = null;
+                        handled = true;
+                    }
                 }
             }
             return handled;
@@ -139,51 +205,101 @@ class WebviewManager {
                 Intent i = new Intent(Intent.ACTION_GET_CONTENT);
                 i.addCategory(Intent.CATEGORY_OPENABLE);
                 i.setType("image/*");
-                activity.startActivityForResult(Intent.createChooser(i,"File Chooser"), FILECHOOSER_RESULTCODE);
+                activity.startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
 
             }
 
             // For Android 3.0+
-            public void openFileChooser( ValueCallback uploadMsg, String acceptType ) {
+            public void openFileChooser(ValueCallback uploadMsg, String acceptType) {
                 mUploadMessage = uploadMsg;
                 Intent i = new Intent(Intent.ACTION_GET_CONTENT);
                 i.addCategory(Intent.CATEGORY_OPENABLE);
                 i.setType("*/*");
-               activity.startActivityForResult(
+                activity.startActivityForResult(
                         Intent.createChooser(i, "File Browser"),
                         FILECHOOSER_RESULTCODE);
             }
 
             //For Android 4.1
-            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture){
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                if (mUploadMessage != null) {
+                    mUploadMessage.onReceiveValue(null);
+                }
                 mUploadMessage = uploadMsg;
-                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-                i.addCategory(Intent.CATEGORY_OPENABLE);
-                i.setType("image/*");
-                activity.startActivityForResult( Intent.createChooser( i, "File Chooser" ), FILECHOOSER_RESULTCODE );
 
+                if("iamge/*".equals(acceptType)){
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    if (takePictureIntent.resolveActivity(activity.getPackageManager()) != null) {
+                        File photoFile = null;
+                        try {
+                            photoFile = createImageFile();
+                            takePictureIntent.putExtra("PhotoPath", mCameraPhotoPath);
+                        } catch (IOException ex) {
+                            Log.e("TAG", "Unable to create Image File", ex);
+                        }
+                        if (photoFile != null) {
+                            mCameraPhotoPath = "file:" +
+                                    photoFile.getAbsolutePath();
+                            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                                    Uri.fromFile(photoFile));
+                        } else {
+                            takePictureIntent = null;
+                        }
+                        activity.startActivityForResult(takePictureIntent, INPUT_FILE_REQUEST_CODE);
+                    }
+                } else if("video/*".equals(acceptType)) {
+                    Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                    if (takeVideoIntent.resolveActivity(activity.getPackageManager()) != null) {
+                        activity.startActivityForResult(takeVideoIntent, INPUT_VIDEO_CODE);
+                    }
+                }
+//                activity.startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
             }
 
             //For Android 5.0+
             public boolean onShowFileChooser(
                     WebView webView, ValueCallback<Uri[]> filePathCallback,
-                    FileChooserParams fileChooserParams){
-                if(mUploadMessageArray != null){
+                    FileChooserParams fileChooserParams) {
+                if (mUploadMessageArray != null) {
                     mUploadMessageArray.onReceiveValue(null);
                 }
                 mUploadMessageArray = filePathCallback;
 
-                Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
-                contentSelectionIntent.setType("*/*");
-                Intent[] intentArray;
-                intentArray = new Intent[0];
-
-                Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
-                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
-                chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser");
-                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
-                activity.startActivityForResult(chooserIntent, FILECHOOSER_RESULTCODE);
+                String[] acceptTypes = fileChooserParams.getAcceptTypes();
+                if (acceptTypes[0].equals("image/*")) {
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(activity.getPackageManager()) != null) {
+                        File photoFile = null;
+                        try {
+                            photoFile = createImageFile();
+                            takePictureIntent.putExtra("PhotoPath", mCameraPhotoPath);
+                        } catch (IOException ex) {
+                            Log.e("TAG", "Unable to create Image File", ex);
+                        }
+                        //适配7.0
+                        if (Build.VERSION.SDK_INT > M) {
+                            if (photoFile != null) {
+                                photoURI = FileProvider.getUriForFile(activity, BuildConfig.APPLICATION_ID + ".fileprovider", photoFile);
+                                takePictureIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                            }
+                        } else {
+                            if (photoFile != null) {
+                                mCameraPhotoPath = "file:" + photoFile.getAbsolutePath();
+                                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                            } else {
+                                takePictureIntent = null;
+                            }
+                        }
+                    }
+                    activity.startActivityForResult(takePictureIntent,INPUT_FILE_REQUEST_CODE);
+                } else if (acceptTypes[0].equals("video/*")) {
+                    Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                    if (takeVideoIntent.resolveActivity(activity.getPackageManager()) != null) {
+                        activity.startActivityForResult(takeVideoIntent, INPUT_VIDEO_CODE);
+                    }
+                }
                 return true;
             }
         });
@@ -229,7 +345,7 @@ class WebviewManager {
         webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(supportMultipleWindows);
 
         webView.getSettings().setSupportMultipleWindows(supportMultipleWindows);
-
+        webView.getSettings().setAllowFileAccess(true);
         webView.getSettings().setAppCacheEnabled(appCacheEnabled);
 
         webView.getSettings().setAllowFileAccessFromFileURLs(allowFileURLs);
@@ -238,6 +354,8 @@ class WebviewManager {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
         }
+
+        webView.getSettings().setDisplayZoomControls(false);
 
         if (clearCache) {
             clearCache();
@@ -255,7 +373,7 @@ class WebviewManager {
             webView.getSettings().setUserAgentString(userAgent);
         }
 
-        if(!scrollBar){
+        if (!scrollBar) {
             webView.setVerticalScrollBarEnabled(false);
         }
 
@@ -266,11 +384,11 @@ class WebviewManager {
         }
     }
 
-    void reloadUrl(String url) {
+    void reloadUrl (String url){
         webView.loadUrl(url);
     }
 
-    void close(MethodCall call, MethodChannel.Result result) {
+    void close (MethodCall call, MethodChannel.Result result){
         if (webView != null) {
             ViewGroup vg = (ViewGroup) (webView.getParent());
             vg.removeView(webView);
@@ -284,12 +402,12 @@ class WebviewManager {
         FlutterWebviewPlugin.channel.invokeMethod("onDestroy", null);
     }
 
-    void close() {
+    void close () {
         close(null, null);
     }
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
-    void eval(MethodCall call, final MethodChannel.Result result) {
+    void eval (MethodCall call,final MethodChannel.Result result){
         String code = call.argument("code");
 
         webView.evaluateJavascript(code, new ValueCallback<String>() {
@@ -300,59 +418,73 @@ class WebviewManager {
         });
     }
     /**
-    * Reloads the Webview.
-    */
-    void reload(MethodCall call, MethodChannel.Result result) {
+     * Reloads the Webview.
+     */
+    void reload (MethodCall call, MethodChannel.Result result){
         if (webView != null) {
             webView.reload();
         }
     }
     /**
-    * Navigates back on the Webview.
-    */
-    void back(MethodCall call, MethodChannel.Result result) {
+     * Navigates back on the Webview.
+     */
+    void back (MethodCall call, MethodChannel.Result result){
         if (webView != null && webView.canGoBack()) {
             webView.goBack();
         }
     }
     /**
-    * Navigates forward on the Webview.
-    */
-    void forward(MethodCall call, MethodChannel.Result result) {
+     * Navigates forward on the Webview.
+     */
+    void forward (MethodCall call, MethodChannel.Result result){
         if (webView != null && webView.canGoForward()) {
             webView.goForward();
         }
     }
 
-    void resize(FrameLayout.LayoutParams params) {
+    void resize (FrameLayout.LayoutParams params){
         webView.setLayoutParams(params);
     }
     /**
-    * Checks if going back on the Webview is possible.
-    */
-    boolean canGoBack() {
+     * Checks if going back on the Webview is possible.
+     */
+    boolean canGoBack () {
         return webView.canGoBack();
     }
     /**
-    * Checks if going forward on the Webview is possible.
-    */
-    boolean canGoForward() {
+     * Checks if going forward on the Webview is possible.
+     */
+    boolean canGoForward () {
         return webView.canGoForward();
     }
-    void hide(MethodCall call, MethodChannel.Result result) {
+    void hide (MethodCall call, MethodChannel.Result result){
         if (webView != null) {
             webView.setVisibility(View.INVISIBLE);
         }
     }
-    void show(MethodCall call, MethodChannel.Result result) {
+    void show (MethodCall call, MethodChannel.Result result){
         if (webView != null) {
             webView.setVisibility(View.VISIBLE);
         }
     }
 
-    void stopLoading(MethodCall call, MethodChannel.Result result){
-        if (webView != null){
+    void stopLoading (MethodCall call, MethodChannel.Result result){
+        if (webView != null) {
             webView.stopLoading();
         }
     }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.CHINA).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName, /* 前级 */
+                ".jpg",    /* 后级 */
+                storageDir    /* 文件夹 */
+        );
+        mCameraPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
 }
